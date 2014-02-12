c3ae563e84953589e390691c52d9eeab22c6fa75
Smith, Cooper <cooper.smith@jwt.com>
2014-02-12 13:05:56 -0500

    add an 'active' class - need to add it to one item at a time not all

--- js/script.js
+++ js/script.js
@@ -17,10 +17,11 @@ function Ctrl($scope) {
 
 
 function DataCtrl($scope, $http) {
-   $scope.newTask = 'New Task';
+   $scope.isActive = false;
 
    $scope.code = null;
    $scope.response = null;
+
 
 
     $http.jsonp('http://sonyainc.net/json/get_sql_data.php?format=jsonp&callback=JSON_CALLBACK').success(function(data, status) {
@@ -32,13 +33,9 @@ function DataCtrl($scope, $http) {
       error(function(data, status) {
         $scope.data = data || "Request failed";
         $scope.status = status;
-    });
-   
-   
-    $scope.getData = function() {
-       // alert("get data: "+ $scope.data[0].task );
-      }
-      
+    });//end jsonp
+
+// Data manipulation routines ( put, delete, refresh)....   
       $scope.putData = function(taskname) {
         alert("put data: " +  taskname);
         $scope.newTask = '';
@@ -52,20 +49,45 @@ function DataCtrl($scope, $http) {
             $scope.data = data || " Delete Request failed";
             $scope.status = status;
         });
-      }
+      }//end putData
 
 
       $scope.deleteData = function(taskId) {
           $http.jsonp('http://sonyainc.net/json/delete_item.php?taskid='+taskId +'&format=jsonp&callback=JSON_CALLBACK').success(function(data, status) {
             $scope.data = data;
             $scope.status = status;
+
+            $scope.isActive = !$scope.isActive;
         }).
           error(function(data, status) {
+             $scope.isActive = true; //!$scope.isActive;
+             alert('alert 2: ' + $scope.isActive);
             $scope.data = data || " Delete Request failed";
             $scope.status = status;
         });
    
-      }
+      }//end deleteData
 
+       $scope.refreshData = function () {
+          $http.jsonp('http://sonyainc.net/json/get_sql_data.php?format=jsonp&callback=JSON_CALLBACK').success(function(data, status) {
+            $scope.data = data;
+            $scope.status = status;
+
+            $scope.total = data.length;
+
+            $scope.isActive = !$scope.isActive;
+        }).
+          error(function(data, status) {
+            $scope.data = data || "Request failed";
+            $scope.status = status;
+        });
+      };//end refreshData
+      //when a link is clicked mark for deletion 
+
+     $scope.toggleActive = function () {
+        $scope.isActive = !$scope.isActive;
+      };
+      
+   
 }//end DataCtrl controller 
  
